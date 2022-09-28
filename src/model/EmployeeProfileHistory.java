fba678f10a5b58094898c2c581d82be3f0c04edd
Mani-NEU <thikkavarapu.m@northeastern.edu>
2022-09-27 20:25:20 -0400

    UI Frame Creation

--- src/model/EmployeeProfileHistory.java
+++ src/model/EmployeeProfileHistory.java
@@ -4,101 +4,26 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author tmani
  */
 public class EmployeeProfileHistory {
     
-    private String name;
-    private Integer employeeId;
-    private Integer age;
-    private String gender;
-    private String startDate;
-    private String level;
-    private String teamInfo;
-    private String positionTitle;
-    private Integer cellPhoneNumber;
-    private String emailAddress;
+    private ArrayList<EmployeeProfile> history;
+    
+    public EmployeeProfileHistory() {
+    this.history = new ArrayList<EmployeeProfile>();
+}
 
-    public String getName() {
-        return name;
+    public ArrayList<EmployeeProfile> getHistory() {
+        return history;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Integer employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getTeamInfo() {
-        return teamInfo;
-    }
-
-    public void setTeamInfo(String teamInfo) {
-        this.teamInfo = teamInfo;
-    }
-
-    public String getPositionTitle() {
-        return positionTitle;
-    }
-
-    public void setPositionTitle(String positionTitle) {
-        this.positionTitle = positionTitle;
-    }
-
-    public Integer getCellPhoneNumber() {
-        return cellPhoneNumber;
-    }
-
-    public void setCellPhoneNumber(Integer cellPhoneNumber) {
-        this.cellPhoneNumber = cellPhoneNumber;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
+    public void setHistory(ArrayList<EmployeeProfile> history) {
+        this.history = history;
     }
     
 }
