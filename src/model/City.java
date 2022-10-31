10aabe12f49a442c8ef7ee9e4080f314c1665c68
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-10-30 23:18:32 -0400

    Last Change

--- src/model/City.java
+++ src/model/City.java
@@ -14,4 +14,29 @@ public class City {
     private String id;
     private String name;
     private ArrayList<Community> communities;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Community> getCommunities() {
+        return communities;
+    }
+
+    public void setCommunities(ArrayList<Community> communities) {
+        this.communities = communities;
+    }
+    
 }
